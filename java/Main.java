8e4b9c7a520dd2a4e86a13c07cabb118e64e7b9c
NamizataS <nsangare@icloud.com>
2020-04-17 19:52:15 +0200

    18 test ok

--- java/Main.java
+++ java/Main.java
@@ -7,7 +7,8 @@ public class Main {
     public static void main(String[] args) {
         RNG.setImpl(new RNGImpl());
 
-        Roll r = new Roll("2d6-2");
+        //Roll r = new Roll("2d6-2");
+        Roll r = new Roll(2,6,-1);
         r.makeRoll(Roll.RollType.NORMAL);
     }
 }
--- java/src/Roll.java
+++ java/src/Roll.java
@@ -1,7 +1,9 @@
 package src;
 
-
 public class Roll {
+    int diceValue;
+    int nbRoll;
+    int modifier;
 
     public enum RollType {
         NORMAL,
@@ -11,17 +13,77 @@ public class Roll {
 
     // Attributes
 
+
     public Roll(String formula) {
         // TODO
+        String regex = "^[0-9]*d[0-9]+$";
+        String regexModifier = "^[0-9]*d[0-9]+[-+][0-9]+$";
+
+        if ( formula.matches( regex ) ) {
+            String[] newFormula = formula.split("d");
+            if ( newFormula[0].isEmpty() ){
+                this.nbRoll = 1;
+            }
+            else {
+                this.nbRoll = Integer.parseInt(newFormula[0] );
+            }
+            this.diceValue = Integer.parseInt( newFormula[1] );
+        }
+
+        if ( formula.matches( regexModifier ) ){
+            String[] secondFormula = formula.split("d");
+            if (secondFormula[0].isEmpty()) {
+                this.nbRoll = 1;
+            }
+            else {
+                this.nbRoll = Integer.parseInt(secondFormula[0]);
+            }
+            this.diceValue = Integer.parseInt( secondFormula[1] );
+            if ( secondFormula[2].equals("-") ){
+                this.modifier = Integer.parseInt(secondFormula[3]);
+                this.modifier *= -1;
+            }
+            else {
+                this.modifier = Integer.parseInt(secondFormula[3]);
+            }
+        }
+
     }
 
     public Roll(int diceValue, int nbRoll, int modifier) {
         // TODO
+        this.diceValue = diceValue;
+        this.nbRoll = nbRoll;
+        this.modifier = modifier;
+
     }
 
     public int makeRoll(RollType rollType) {
         // TODO
-        return 0;
+        int counter=0;
+        if ( this.diceValue <= 0 ){
+            return -1;
+        }
+        if ( this.nbRoll <= 0 ){
+            return -1;
+        }
+        for ( int i = 0; i < this.nbRoll; i++ ){
+            Dice dice = new Dice( this.diceValue );
+            int roll1 = dice.rollDice();
+            int roll2 = dice.rollDice();
+            if ( rollType == RollType.NORMAL ){
+                counter += roll1;
+            }
+            if ( rollType == RollType.ADVANTAGE ){
+                counter += Math.max(roll1, roll2);
+            }
+            if ( rollType == RollType.DISADVANTAGE){
+                counter += Math.min(roll1, roll2);
+            }
+        }
+        counter += this.modifier;
+
+        return Math.max(counter, 0);
     }
 
 }
