901b9cf19fec65dea3d7d8d9f306bd4086b61860
Tom <tom.robert.pro@gmail.com>
2020-04-24 19:37:17 +0200

    Test de commit

--- java/Main.java
+++ java/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         RNG.setImpl(new RNGImpl());
-
+        efzzefze
         Roll r = new Roll("2d6-2");
         r.makeRoll(Roll.RollType.NORMAL);
     }
